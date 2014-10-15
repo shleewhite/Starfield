1fa0c1db8f1e1fde60a42ea384e4fab2cc857140
shleewhite <lwhite@sportsbasement.com>
2014-10-15 12:53:19 -0700

    made it awesome

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] starz;
-void setup()
+public void setup()
 {
 	size(1000, 900);
 	background(0);
@@ -14,7 +30,7 @@ void setup()
 	starz[starz.length-4] = new OddballParticle();
 	starz[starz.length-5] = new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	if ((int)((NormalParticle)starz[0]).dX == 500)
 	{
@@ -44,7 +60,7 @@ class NormalParticle implements Particle
 		{
 			dX = dX + Math.cos(dTheta)*dSpeed;
 			dY = dY + Math.sin(dTheta)*dSpeed;
-			dTheta += 0.01;
+			dTheta += 0.01f;
 		}
 	public void show()
 	{
@@ -73,7 +89,7 @@ class OddballParticle implements Particle
 	{
 		dXX = dXX + (int)(Math.random()*5)-2;
 		dYY = dYY + Math.cos(dThetaO)*dSpeedO;
-		dThetaO += 0.05;
+		dThetaO += 0.05f;
 	}
 	public void show()
 	{
@@ -81,4 +97,13 @@ class OddballParticle implements Particle
 		fill((int)Math.random()*256, 192, 192);
 		ellipse((float)dXX, (float)dYY, 15, 15);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
